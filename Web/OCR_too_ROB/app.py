3c8f02d2efe219bbaa1c549809dec170f831120f
Charles Truluck <github@ctruluck.com>
2020-02-11 14:29:41 -0500

    fixed OCR

--- Web/OCR_too_ROB/app.py
+++ Web/OCR_too_ROB/app.py
@@ -8,6 +8,8 @@ import pytesseract
 import argparse
 import cv2
 import os
+import re
+
 app = Flask(__name__)
 UPLOAD_FOLDER = '/tmp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -32,12 +34,16 @@ def upload_file():
       f.save(filepath)
 
       pipe = Popen(pytesseract.pytesseract.tesseract_cmd + ' ' + filepath + ' stdout', shell=True, stdout=PIPE, stderr=PIPE)
-      output = str(pipe.stdout.read()).replace("\n", "").replace("\r", "")
-      p = Popen('echo "' + output + '" | grep allowed', shell=True, stdout=PIPE, stderr=PIPE)
+      output = pipe.stdout.read().decode('utf-8').replace("\n", "").replace("\r", "")
+      output = re.sub(r'[^\x00-\x7f]',r'', output)
+      p = Popen('echo ' + output + ' | grep allowed', shell=True, stdout=PIPE, stderr=PIPE)
       stdout, stderr = p.communicate()
-
-
-      return render_template("index.html", info=output, error=str(stderr))
+      print(output)
+      err = stderr.decode('utf-8').replace("\n", "").replace("\r", "")
+      err = re.sub(r'[^\x00-\x7f]',r'', err)
+      s_out = stdout.decode('utf-8').replace("\n", "").replace("\r", "")
+      s_out =  re.sub(r'[^\x00-\x7f]',r'', s_out)
+      return render_template("index.html", info=output, error=(err + '  ' + s_out))
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5000, debug=True)
